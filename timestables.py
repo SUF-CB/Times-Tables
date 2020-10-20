bfb7015d8b4f58e669117580dec730b0c2144a81
SUF-CB <54987708+SUF-CB@users.noreply.github.com>
2020-10-20 10:55:24 +0100

    Create timestables.py

---
+++ timestables.py
@@ -0,0 +1,14 @@
+name = input("What's your name?")# First the user will get asked to input their name.
+print("Hi, " + name + "!")# welcomes user
+def Loop():
+     num = int(input(" please enter a number of your choice to find the timestables of "+ name + ">>"))
+     print("Multiplication Table of", num)
+
+     for i in range(1, 13):
+      print(num,"X",i,"=",num * i)
+     repeat = input("Do You want to enter another number? Yes or No >> ")#The user will be asked to input eihter Yes/NO if they want to input another number.
+     if repeat == "Yes": # if the user inputs Yes as their answer it will output the message and praise the user by saying well done along with their  name.
+                Loop()
+     else: #  if the user inputs No it will end and close the program automatically.
+      exit()# The user has said "No".Therefore, the program will close with the exit function. 
+Loop()
